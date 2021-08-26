aecbbd3fded26e7eb4818ffbf03109692d7a61e9
Aseem Chopra <aseemchopra@protonmail.com>
2021-08-26 15:21:20 +0530

    ✨ initialised first iteration of spinner

---
+++ src/loader.js
@@ -0,0 +1,15 @@
+import Loader from "react-loader-spinner";
+const LoaderComp = () =>{
+    const style={position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"};
+    return(
+        <Loader
+        type="TailSpin"
+        color="rgb(51,51,51)"
+        height={70}
+        width={70}
+        timeout={5000}
+        style={style}
+        />
+    );
+}
+export default LoaderComp;
