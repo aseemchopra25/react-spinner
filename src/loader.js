6b968740d6737e7ec2dd6ff5f5c74be397823101
Aseem Chopra <aseemchopra@protonmail.com>
2021-08-26 15:58:43 +0530

    ✨ styling, alignment and color

--- src/loader.js
+++ src/loader.js
@@ -3,7 +3,7 @@ const LoaderComp = () =>{
     const style={position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"};
     return(
         <Loader
-        type="TailSpin"
+        type="Grid"
         color="rgb(51,51,51)"
         height={70}
         width={70}
